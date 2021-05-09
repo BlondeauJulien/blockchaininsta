183b86d445b1eacd35e3cf8863a327924c793224
Julien Blondeau <julienblondeau@live.fr>
2021-05-09 15:14:32 +0200

    create an image

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,34 @@
 pragma solidity ^0.5.0;
 
 contract Decentragram {
-  // Code goes here...
+    string public name = "Decentragram";
+    // Store Images
+    uint256 public imageCount = 0;
+    mapping(uint256 => Image) public images;
+
+    struct Image {
+        uint256 id;
+        string hash;
+        string description;
+        uint256 tipAmount;
+        address payable author;
+    }
+
+    // Create Images
+    function uploadImage(string memory _imgHash, string memory _description)
+        public
+    {
+        // Increment image id
+        imageCount = imageCount++;
+        //add image to contract
+        images[imageCount] = Image(
+            imageCount,
+            _imgHash,
+            _description,
+            0,
+            msg.sender // msg.sender is the person that send this request (their etherum address)
+        );
+    }
+
+    //Tip Images
 }
